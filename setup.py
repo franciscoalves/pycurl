abedadf4138fdde8c770155eac150b6bc735044a
Francisco Alves <chico@corp.globo.com>
2014-07-30 17:16:36 -0300

    detects NSS without the need of --with-nss

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ class ExtensionConfiguration(object):
         self.extra_objects = []
         self.extra_compile_args = []
         self.extra_link_args = []
-        
+
         self.configure()
 
     @property
@@ -174,7 +174,7 @@ class ExtensionConfiguration(object):
             if errtext:
                 msg += ":\n" + errtext
             raise ConfigurationError(msg)
-        
+
         ssl_lib_detected = False
         if 'PYCURL_SSL_LIBRARY' in os.environ:
             ssl_lib = os.environ['PYCURL_SSL_LIBRARY']
@@ -225,6 +225,10 @@ class ExtensionConfiguration(object):
                     self.define_macros.append(('HAVE_CURL_NSS', 1))
                     ssl_lib_detected = True
                     self.libraries.append('ssl3')
+                if not ssl_lib_detected and self.nss_detected():
+                    self.define_macros.append(('HAVE_CURL_NSS', 1))
+                    ssl_lib_detected = True
+                    self.libraries.append('ssl3')
         if not ssl_lib_detected:
             p = subprocess.Popen((CURL_CONFIG, '--features'),
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -244,11 +248,11 @@ class ExtensionConfiguration(object):
             self.define_macros.append(('HAVE_CURL_SSL', 1))
         if not self.libraries:
             self.libraries.append("curl")
-        
+
         # Add extra compile flag for MacOS X
         if sys.platform[:-1] == "darwin":
             self.extra_link_args.append("-flat_namespace")
-        
+
         # Recognize --avoid-stdio on Unix so that it can be tested
         self.check_avoid_stdio()
 
@@ -286,9 +290,9 @@ class ExtensionConfiguration(object):
         if not os.path.exists(libcurl_lib_path):
             fail("libcurl.lib does not exist at %s.\nCurl directory must point to compiled libcurl (bin/include/lib subdirectories): %s" %(libcurl_lib_path, curl_dir))
         self.extra_objects.append(libcurl_lib_path)
-        
+
         self.check_avoid_stdio()
-        
+
         # make pycurl binary work on windows xp.
         # we use inet_ntop which was added in vista and implement a fallback.
         # our implementation will not be compiled with _WIN32_WINNT targeting
@@ -312,8 +316,35 @@ class ExtensionConfiguration(object):
         configure = configure_windows
     else:
         configure = configure_unix
-    
-    
+
+    def nss_detected(self):
+        curl_version_info = self.get_curl_version_info()
+        return "NSS/" in curl_version_info
+
+    def get_curl_version_info(self):
+        import ctypes
+        from ctypes import util
+        clib = ctypes.CDLL(util.find_library('curl'))
+        curl_version = self.get_function_from_clib(
+            clib,
+            'curl_version',
+            restype=ctypes.c_char_p
+        )
+
+        return curl_version()
+
+    def get_function_from_clib(self, clib, func_name, restype=None, errcheck=None):
+        def not_implemented_func():
+            raise NotImplementedError
+
+        func = getattr(clib, func_name, not_implemented_func)
+        if restype:
+            func.restype = restype
+        if errcheck:
+            func.errcheck = errcheck
+
+        return func
+
     def check_avoid_stdio(self):
         if 'PYCURL_SETUP_OPTIONS' in os.environ and '--avoid-stdio' in os.environ['PYCURL_SETUP_OPTIONS']:
             self.extra_compile_args.append("-DPYCURL_AVOID_STDIO")
@@ -329,7 +360,7 @@ def get_bdist_msi_version_hack():
     import inspect
     import types
     import re
-    
+
     class bdist_msi_version_hack(bdist_msi):
         """ MSI builder requires version to be in the x.x.x format """
         def run(self):
@@ -350,7 +381,7 @@ def get_bdist_msi_version_hack():
             self.distribution.metadata.get_version = \
                 types.MethodType(monkey_get_version, self.distribution.metadata)
             bdist_msi.run(self)
-    
+
     return bdist_msi_version_hack
 
 
@@ -445,7 +476,7 @@ def get_data_files():
 
 def check_manifest():
     import fnmatch
-    
+
     f = open('MANIFEST.in')
     globs = []
     try:
@@ -458,7 +489,7 @@ def check_manifest():
             globs.append(glob)
     finally:
         f.close()
-    
+
     paths = []
     start = os.path.abspath(os.path.dirname(__file__))
     for root, dirs, files in os.walk(start):
@@ -469,7 +500,7 @@ def check_manifest():
                 continue
             rel = os.path.join(root, file)[len(start)+1:]
             paths.append(rel)
-    
+
     for path in paths:
         included = False
         for glob in globs:
@@ -487,11 +518,11 @@ def check_authors():
         contents = f.read()
     finally:
         f.close()
-    
+
     paras = contents.split("\n\n")
     authors_para = paras[AUTHORS_PARAGRAPH]
     authors = [author for author in authors_para.strip().split("\n")]
-    
+
     log = subprocess.check_output(['git', 'log', '--format=%an (%ae)'])
     for author in log.strip().split("\n"):
         author = author.replace('@', ' at ').replace('(', '<').replace(')', '>')
@@ -511,7 +542,7 @@ def convert_docstrings():
     for entry in sorted(os.listdir('doc/docstrings')):
         if not entry.endswith('.rst'):
             continue
-        
+
         name = entry.replace('.rst', '')
         f = open('doc/docstrings/%s' % entry)
         try:
@@ -636,7 +667,7 @@ if __name__ == "__main__":
             split_extension_source = True
         ext = get_extension(split_extension_source=split_extension_source)
         setup_args['ext_modules'] = [ext]
-        
+
         for o in ext.extra_objects:
             assert os.path.isfile(o), o
         setup(**setup_args)
